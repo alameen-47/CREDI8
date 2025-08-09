89db9ee8131b5afae3f139909f977cdb807f5335
alameen-47 <alameenkhan1431@gmail.com>
2025-08-09 14:31:22 +0300

    backend url updated

--- backend/api/api.js
+++ backend/api/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Use 10.0.2.2 for Android emulator, localhost or your IP for iOS simulator or physical device
 const api = axios.create({
-  baseURL: 'http://10.0.2.2:8086', // 
+  baseURL: process.env.REACT_APP_API_URL || 'http://10.0.2.2:8086',
 });
 
 export default api;
